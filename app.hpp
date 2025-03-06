d09107387f1a27ef48233529c3c2542c2ab58e0e
Vojtěch Gerö <vojtech.gero@tul.cz>
2025-03-06 14:36:40 +0100

    Triangle (#1)
    * it does not work
    
    * it triangles (works) now

--- app.hpp
+++ app.hpp
@@ -1,23 +1,21 @@
-#include "assets.hpp"
+#include <GL/glew.h>
 #include <vector>
-
+#include "assets.hpp"
 class App {
+public:
+    bool init();
+    void init_assets();
+    void run(GLFWwindow* window);
+    ~App();
 
 private:
-
-    //new GL stuff
     GLuint shader_prog_ID{ 0 };
     GLuint VBO_ID{ 0 };
     GLuint VAO_ID{ 0 };
-
     GLfloat r{ 1.0f }, g{ 0.0f }, b{ 0.0f }, a{ 1.0f };
-
-    std::vector<vertex> triangle_vertices =
-    {
+    std::vector<vertex> triangle_vertices = {
         {{0.0f,  0.5f,  0.0f}},
         {{0.5f, -0.5f,  0.0f}},
         {{-0.5f, -0.5f,  0.0f}}
     };
-
 };
-
